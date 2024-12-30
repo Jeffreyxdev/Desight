7e2b5abdd4d344a6015ba2f2d1e4200a52461497
jeffrey <agabaenwerejeffrey@gmail.com>
2024-12-30 19:35:38 +0000

    q

--- src/InnerPages/Slider.js
+++ src/InnerPages/Slider.js
@@ -1,26 +1,31 @@
 export const SliderData = [
 {
     image:
-    '../src/assets/images (1).png'
+    'https://github.com/Jeffreyxdev/Desight/blob/main/src/assets/images%20(1).png'
 },
 {
     image:
-    '../src/assets/images (3).jpg'
+    'https://github.com/Jeffreyxdev/Desight/blob/main/src/assets/Desight(1).jpg'
 },
 {
     image:
-    '../src/assets/hidden.jpg'
+    'https://github.com/Jeffreyxdev/Desight/blob/main/src/assets/images%20(3).jpg'
 },
 {
     image:
-    '../src/assets/Desight(1).jpg'
+    'https://magnaeyecare.com/wp-content/uploads/2024/06/Optician_Abuja-01.jpg'
 },
 {
     image:
-    '../src/assets/Desight(2).jpg'
+    'https://github.com/Jeffreyxdev/Desight/blob/main/src/assets/hidden.jpg'
 },
+
 {
     image:
-    '../src/assets/images (2).jpg'
+    'https://github.com/Jeffreyxdev/Desight/blob/main/src/assets/Desight(2).jpg'
 },
+// {
+//     image:
+//     ''
+// },
 ];
